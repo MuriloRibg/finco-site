8db6de2b0e9965b5247725e9df7a83cfc1c1f22b
Murilo Ribeiro <88835239+MuriloRibg@users.noreply.github.com>
2024-05-01 11:59:42 -0300

    Add spinner

--- finco-site/src/app/app.module.ts
+++ finco-site/src/app/app.module.ts
@@ -2,15 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { LoginModule } from './login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
   ],
   imports: [
-    BrowserModule
+    LoginModule,
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
