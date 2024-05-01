ac1b6141fbbf63505a157f63f82c3413acd521c8
Murilo Ribeiro <88835239+MuriloRibg@users.noreply.github.com>
2024-05-01 11:59:26 -0300

    Create environments

--- finco-site/src/app/api/login/service/login.service.ts
+++ finco-site/src/app/api/login/service/login.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { LoginRequest } from '../models/request/login-request';
-import { LoginResponse } from '../models/response/login-response';
 
-const API = "http://localhost:3000/login"
+import { environment } from 'src/environments/environment';
+import { LoginRequest } from 'src/app/api/login/models/request/login-request';
+import { LoginResponse } from 'src/app/api/login/models/response/login-response';
+
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private readonly url = `${environment.api}/login`
 
   constructor(private readonly http: HttpClient) { }
 
   login(request: LoginRequest): Observable<LoginResponse> {
-    return this.http.post<LoginResponse>(API, request);
+    return this.http.post<LoginResponse>(this.url, request);
   }
-  
 }
