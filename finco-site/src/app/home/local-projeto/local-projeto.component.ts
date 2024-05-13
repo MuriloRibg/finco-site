57655d95aa76f09bc7787fe767a1cd9d8ba29546
Gisella Costa <gisellacostauvv@gmail.com>
2024-05-13 17:43:26 -0300

    Criada tela de cadastro

--- finco-site/src/app/home/local-projeto/local-projeto.component.ts
+++ finco-site/src/app/home/local-projeto/local-projeto.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-local-projeto',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./local-projeto.component.css']
 })
 export class LocalProjetoComponent {
+  constructor(private readonly router: Router){}
 
+  mudarTela(): void {
+    this.router.navigate(["/subcategoria"]);
+  }
+
+  mudarProjeto(): void {
+    this.router.navigate(["/projeto"]);
+  }
 }
